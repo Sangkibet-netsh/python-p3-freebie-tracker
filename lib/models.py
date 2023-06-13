5014bd79d194770db486280a5e367e0e8b072596
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-06-13 13:03:54 +0300

    Complete

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,9 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Table
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+
+
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -8,6 +11,14 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+dev_company_association = Table(
+    'dev_company_association',
+    Base.metadata,
+    Column('dev_id', Integer, ForeignKey('dev.id')),
+    Column('company_id', Integer, ForeignKey('companies.id'))
+)
+
 
 class Company(Base):
     __tablename__ = 'companies'
@@ -19,11 +30,48 @@ class Company(Base):
     def __repr__(self):
         return f'<Company {self.name}>'
 
+
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+
+    @property
+    def freebies(self):
+        return self.freebies
+
+    @property
+    def companies(self):
+        return [freebie.company for freebie in self.freebies]
+
+    def received_one(self, item_name):
+        return any(freebie.item_name == item_name for freebie in self.freebies)
+
+    def give_away(self, dev, freebie):
+        if freebie.dev == self:
+            freebie.dev = dev
+
+
+class Freebie(Base):
+    __tablename__ = 'freebies'
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String, nullable=False)
+    value = Column(Integer())
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    Company_id = Column(Integer, ForeignKey('companies.id'))
+
+    dev = relationship('Dev', backref=backref(
+        'freebies', cascade='all,delete-orphan'))
+    company = relationship('Company', backref=backref(
+        'freebies', cascade='all, delete-orphan'))
+
+    def __repr__(self):
+        return f'<Freebie {self.id}, {self.item_name}, {self.value}, {self.dev_id}, {self.company_id}>'
+
+    def print_details(self):
+        return f'{self.dev.name} owns a {self.item_name} from {self.company.name}'
+    
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,55 @@
 #!/usr/bin/env python3
 
 # Script goes here!
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Company, Dev, Freebie, Base
+
+# Database connection
+engine = create_engine('sqlite:///database/freebies.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Create tables if they don't exist
+Base.metadata.create_all(bind=engine)
+
+# Sample data
+companies_data = [
+    {'name': 'Company X', 'founding_year': 2005},
+    {'name': 'Company Y', 'founding_year': 2012},
+    {'name': 'Company Z', 'founding_year': 1998}
+]
+
+dev_data = [
+    {'name': 'Developer 1'},
+    {'name': 'Developer 2'},
+    {'name': 'Developer 3'}
+]
+
+freebies_data = [
+    {'item_name': 'T-shirt', 'value': 20, 'dev_id': 1, 'company_id': 1},
+    {'item_name': 'Mug', 'value': 10, 'dev_id': 2, 'company_id': 2},
+    {'item_name': 'Sticker', 'value': 5, 'dev_id': 3, 'company_id': 3},
+]
+
+# Populate companies
+for company_data in companies_data:
+    company = Company(**company_data)
+    session.add(company)
+
+# Populate devs
+for dev_data in dev_data:
+    dev = Dev(**dev_data)
+    session.add(dev)
+
+# Populate freebies
+for freebie_data in freebies_data:
+    freebie = Freebie(**freebie_data)
+    session.add(freebie)
+
+# Commit the changes
+session.commit()
+
+# Close the session
+session.close()
